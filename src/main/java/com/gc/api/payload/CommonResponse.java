4bbad59c3a1f19b2f6e44bedc56edce98589827d
Eunji Kim <kej431003@gmail.com>
2025-11-11 20:19:00 +0900

    [Bug] ObjectMapper 직렬화 에러 해결 (#8)
    ## 💡 관련 이슈 번호
    
    - #7 
    
    ## 📢 작업 내용
    
    - **에러 해결**
      - Spring이 제공하는 `ObjectMapper`를 사용해도록 수정 
    
    ## 🗨️ 코드 리뷰 요구사항 (선택)
    
    -

--- src/main/java/com/gc/api/payload/CommonResponse.java
+++ src/main/java/com/gc/api/payload/CommonResponse.java
@@ -1,15 +1,11 @@
 package com.gc.api.payload;
 
-import java.io.IOException;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gc.api.payload.status.ErrorReason;
 import com.gc.api.payload.status.SuccessStatus;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -17,8 +13,6 @@ import lombok.Getter;
 @AllArgsConstructor
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 public class CommonResponse<T> {
-
-	private static final ObjectMapper mapper = new ObjectMapper();
 
 	@JsonProperty("isSuccess")
 	private final Boolean isSuccess;
@@ -47,17 +41,5 @@ public class CommonResponse<T> {
 	// 요청 실패 - ExceptionAdvice에서 사용
 	public static <T> CommonResponse<T> onFailure(ErrorReason errorReason, T data) {
 		return new CommonResponse<>(false, errorReason.getCode(), errorReason.getMessage(), data);
-	}
-
-	public static void setErrorResponse(HttpServletResponse response,
-		ErrorReason errorReason, String message) throws IOException {
-
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("application/json; charset=UTF-8");
-		response.setStatus(errorReason.getHttpStatus().value());
-
-		CommonResponse<String> res = CommonResponse
-			.onFailure(errorReason, message);
-		response.getWriter().write(mapper.writeValueAsString(res));
 	}
 }
